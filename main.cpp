e70611040b1ee707b7c3db224163631e45188ad3
Milosz Sliwka <milsli@gmail.com>
2024-11-07 12:09:33 +0100

    Początek implementacji wielowątkowości

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
-#include "mainwindow.h"
+// #include "mainwindow.h"
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+//     MainWindow w;
+//     w.show();
+//     return a.exec();
+// }
 
 #include <QApplication>
+#include <iostream>
+#include <QDebug>
 
+using namespace std;
+
+// DOING THINGS IN PARALLEL
+#include <QThread>
+bool stopThreads = false;
+class TextThread : public QThread
+{
+public:
+    TextThread( const QString &text );
+    void run();
+private:
+    QString m_text;
+};
+TextThread::TextThread( const QString &text ) : QThread()
+{
+        m_text = text;
+}
+void TextThread::run()
+{
+    while( !stopThreads )
+    {
+        qDebug() << m_text;
+        sleep( 1 );
+    }
+}
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+
+
+    cout << "\nEveryting is okay\n";
+
+    return 0;
 }
